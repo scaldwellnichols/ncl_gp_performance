0a0d6dc76e9a83486c5a174b8c21fce8b151eda2
CALDWELL-NICHOLS <scott.caldwell-nichols1@nhs.net>
2024-11-24 13:26:55 +0000

    function to get GP practice names from the ODS API

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,5 @@
+import requests
+
 def min_max_normalisation(series):
     """
     Function to normalise a pandas series using min-max normalisation.
@@ -13,3 +15,25 @@ def min_max_normalisation(series):
         The normalised series.
     """
     return (series - series.min()) / (series.max() - series.min())
+
+def get_gp_name(gp_code : str) -> str:
+    """
+    Function to get the name of a GP practice from the NHS Spine Organisation Data Service (ODS) API.
+
+    Parameters
+    ----------
+    gp_code : str
+        The GP practice code to look up.
+
+    Returns
+    -------
+    str
+        The name of the GP practice.
+    """
+    url = f"https://directory.spineservices.nhs.uk/ORD/2-0-0/organisations/{gp_code}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('Organisation', {}).get('Name', 'Unknown')
+    else:
+        return 'Unknown'
